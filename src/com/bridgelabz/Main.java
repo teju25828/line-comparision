dd314b60de33cf6516e98154a53801b7bca0b5a0
Raaj <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>
2022-10-06 19:27:17 +0530

    first commit

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Line lineCompare =  new Line();
+    lineCompare.getData();
+    lineCompare.compareTo();
+    }
+}
